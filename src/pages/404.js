70f7fe4533de66ce0498afa003b93b110a5a21c3
tzetzo <tzvetanmarinov@yahoo.com>
2020-04-13 14:31:21 +0300

    404 styled

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,11 +4,28 @@ import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
+    <div
+      style={{
+        fontSize: '2rem',
+        color: 'orangered',
+        display: 'grid',
+        gridTemplateRows: 'repeat(3,1fr)',
+        justifyItems: 'center',
+        alignItems: 'center',
+      }}
+    >
       <h1>PAGE NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist...</p>
       <p>
-        <Link to="/">Head home</Link>
+        <Link
+          to="/"
+          style={{
+            textDecoration: 'none',
+            color: "blueviolet"
+          }}
+        >
+          Head home
+        </Link>
       </p>
     </div>
   </Layout>
